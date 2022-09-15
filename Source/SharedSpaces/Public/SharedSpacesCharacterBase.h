695ffe62bdd46e5f4fee3562646340a8be0a19c9
els <els@fb.com>
2022-09-15 14:38:06 -0700

    Fixed path to Oculus LICENSE.

--- Source/SharedSpaces/Public/SharedSpacesCharacterBase.h
+++ Source/SharedSpaces/Public/SharedSpacesCharacterBase.h
@@ -1,6 +1,6 @@
 // Copyright (c) Meta Platforms, Inc. and its affiliates.
 // Use of the material below is subject to the terms of the MIT License
-// https://github.com/oculus-samples/Unreal-SharedSpaces/Source/LICENSE
+// https://github.com/oculus-samples/Unreal-SharedSpaces/blob/main/Source/LICENSE
 
 
 #pragma once
